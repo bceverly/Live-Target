1f6c1a628fe7bf2c2f17c74fac08af160887da8f
bceverly <bryan@theeverlys.com>
2025-07-07 10:47:49 -0400

    Initial Commit

---
+++ Live Target/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Live Target
+//
+//  Created by Bryan Everly on 7/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Live TargetTests/Live_TargetTests.swift
@@ -0,0 +1,17 @@
+//
+//  Live_TargetTests.swift
+//  Live TargetTests
+//
+//  Created by Bryan Everly on 7/7/25.
+//
+
+import Testing
+@testable import Live_Target
+
+struct Live_TargetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
