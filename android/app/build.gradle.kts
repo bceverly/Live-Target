508f3f5ff7a429bb1e8b0889f143f47435f8a2c8
bceverly <bryan@theeverlys.com>
2025-07-15 07:42:00 -0400

    Cleaned up code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bceassociates.livetarget"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.93"
+        versionCode = 5
+        versionName = "0.94"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
